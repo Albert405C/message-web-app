6b152bbc0ad7dedf82f723fc3d1700fb82847749
Albert Mitto <chrismitto9@gmail.com>
2023-12-05 14:41:19 +0300

    updates

--- branch/src/App.js
+++ branch/src/App.js
@@ -3,8 +3,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import io from 'socket.io-client';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
-const socket = io('http://localhost:3001');
+const socket = io('http://localhost:3000');
 
 function App() {
   const [messages, setMessages] = useState([]);
@@ -26,59 +27,94 @@ function App() {
       socket.disconnect();
     };
   }, []);
+// Add these functions in src/App.js
 
+const assignMessage = (messageId, agentId) => {
+    axios.post(`http://localhost:3000/messages/assign/${messageId}/${agentId}`)
+      .catch(error => console.error(error));
+  };
+  
+  const lockMessage = (messageId, agentId) => {
+    axios.post(`http://localhost:3000/messages/lock/${messageId}/${agentId}`)
+      .catch(error => console.error(error));
+  };
+  
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setNewMessage(prevMessage => ({ ...prevMessage, [name]: value }));
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  
+   // Update handleSubmit function in src/App.js
 
+const handleSubmit = (e) => {
+    e.preventDefault();
+  
     // Simulate sending a message to the server
-    axios.post('http://localhost:3000/messages', newMessage)
+    axios.post('http://localhost:3000/messages', { ...newMessage, assignedAgent: null, lockedBy: null })
       .then(response => {
         // Clear the form and let Socket.IO handle real-time updates
         setNewMessage({ sender: '', content: '' });
       })
       .catch(error => console.error(error));
   };
-
+  
   return (
-    <div>
-      <h1>Branch Messaging App</h1>
-      <ul>
-        {messages.map(message => (
-          <li key={message._id}>
-            <strong>{message.sender}:</strong> {message.content}
-          </li>
-        ))}
-      </ul>
-
-      {/* Form for sending new messages */}
-      <form onSubmit={handleSubmit}>
-        <label>
-          Sender:
-          <input
-            type="text"
-            name="sender"
-            value={newMessage.sender}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Content:
-          <input
-            type="text"
-            name="content"
-            value={newMessage.content}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <button type="submit">Send Message</button>
-      </form>
+    <div className="container">
+      <h1 className="mt-4 mb-4">Branch Messaging App</h1>
+      <div className="row">
+        <div className="col-md-8">
+          <ul className="list-group">
+          {messages.map(message => (
+  <li key={message._id} className="list-group-item">
+    <strong>{message.sender}:</strong> {message.content}
+    {!message.assignedAgent && (
+      <button
+        className="btn btn-sm btn-success ms-2"
+        onClick={() => assignMessage(message._id, 'agent123')} // Replace 'agent123' with the actual agent ID
+      >
+        Assign
+      </button>
+    )}
+    {message.assignedAgent === 'agent123' && !message.lockedBy && (
+      <button
+        className="btn btn-sm btn-warning ms-2"
+        onClick={() => lockMessage(message._id, 'agent123')} // Replace 'agent123' with the actual agent ID
+      >
+        Lock
+      </button>
+    )}
+  </li>
+))}
+          </ul>
+        </div>
+        <div className="col-md-4">
+          {/* Form for sending new messages */}
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label className="form-label">Sender:</label>
+              <input
+                type="text"
+                className="form-control"
+                name="sender"
+                value={newMessage.sender}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Content:</label>
+              <input
+                type="text"
+                className="form-control"
+                name="content"
+                value={newMessage.content}
+                onChange={handleInputChange}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">Send Message</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -16,14 +16,20 @@ mongoose.connect('mongodb://localhost/branch-messaging-app', {
   useUnifiedTopology: true,
 });
 
-const messageSchema = new mongoose.Schema({
-  sender: String,
-  content: String,
-  timestamp: { type: Date, default: Date.now },
-  isRead: { type: Boolean, default: false },
-});
+// Update messageSchema in server.js
 
-const Message = mongoose.model('Message', messageSchema);
+const messageSchema = new mongoose.Schema({
+    sender: String,
+    content: String,
+    timestamp: { type: Date, default: Date.now },
+    isRead: { type: Boolean, default: false },
+    assignedAgent: { type: String, default: null }, // Store the assigned agent's ID or username
+    lockedBy: { type: String, default: null }, // Store the ID or username of the agent who has locked the message
+  });
+  
+  const Message = mongoose.model('Message', messageSchema);
+  
+
 
 const server = http.createServer(app);
 const io = socketIo(server);
@@ -46,20 +52,69 @@ const seedMessages = async () => {
 seedMessages();
 
 app.get('/messages', async (req, res) => {
-  const messages = await Message.find();
-  res.json(messages);
-});
+    // Set CORS headers in the response
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+  
+    const messages = await Message.find();
+    res.json(messages);
+  });
+// Update /messages POST endpoint in server.js
 
 app.post('/messages', async (req, res) => {
-  const { sender, content } = req.body;
-  const newMessage = new Message({ sender, content });
-  await newMessage.save();
+    const { sender, content, assignedAgent, lockedBy } = req.body;
+    const newMessage = new Message({ sender, content, assignedAgent, lockedBy });
+    await newMessage.save();
+  
+    io.emit('newMessage', newMessage);
+  
+    res.status(201).json(newMessage);
+  });
+  // Add new endpoints in server.js
 
-  io.emit('newMessage', newMessage);
-
-  res.status(201).json(newMessage);
-});
-
+// Assign a message to an agent
+app.post('/messages/assign/:messageId/:agentId', async (req, res) => {
+    const { messageId, agentId } = req.params;
+    const message = await Message.findById(messageId);
+  
+    if (!message) {
+      return res.status(404).json({ error: 'Message not found' });
+    }
+  
+    if (message.lockedBy && message.lockedBy !== agentId) {
+      return res.status(403).json({ error: 'Message is locked by another agent' });
+    }
+  
+    message.assignedAgent = agentId;
+    await message.save();
+  
+    io.emit('assignedMessage', message);
+  
+    res.json(message);
+  });
+  
+  // Lock a message for an agent
+  app.post('/messages/lock/:messageId/:agentId', async (req, res) => {
+    const { messageId, agentId } = req.params;
+    const message = await Message.findById(messageId);
+  
+    if (!message) {
+      return res.status(404).json({ error: 'Message not found' });
+    }
+  
+    if (message.assignedAgent !== agentId) {
+      return res.status(403).json({ error: 'Message is not assigned to the requesting agent' });
+    }
+  
+    message.lockedBy = agentId;
+    await message.save();
+  
+    io.emit('lockedMessage', message);
+  
+    res.json(message);
+  });
+  
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
