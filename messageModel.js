b421ab8e1118bd279bea03931e5a5e2ebd9dbb84
Albert Mitto <chrismitto9@gmail.com>
2023-12-05 18:13:22 +0300

    add

--- messageModel.js
+++ messageModel.js
@@ -1,21 +1,29 @@
 const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
-
-	  sender: {
-		      type: String,
-		      required: true,
-		    },
-	  content: {
-		      type: String,
-		      required: true,
-		    },
-	  timestamp: {
-		      type: Date,
-		      required: true,
-		    },
+  id: {
+    type: Number, // Assuming ID is a number
+    required: true,
+  },
+  date: {
+    type: String, // You can use Date type if you prefer
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    required: true,
+  },
 });
 
 const Message = mongoose.model('Message', messageSchema);
 
 module.exports = Message;
+
